94ecccf35b130a7e3daa011b6acdcbb031466c1f
Caesar Ladion <caesar.ladion@gmail.com>
2019-11-12 16:03:33 -0800

    initial commit

---
+++ events.go
@@ -0,0 +1,20 @@
+// 3 event structs
+// withdraw
+// take down
+// put up
+
+package main
+
+import ()
+
+// to payment
+type WithdrawMoneyEvent struct {
+}
+
+// to catalog
+type TakeAccountDownEvent struct {
+}
+
+// to catalog
+type NewAccountEvent struct {
+}
