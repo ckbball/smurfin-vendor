86913c769b40339aacad7bc29114ac5b99af06a9
Caesar Ladion <caesar.ladion@gmail.com>
2019-11-15 16:17:59 -0800

    added notes and squared away events.go, added journaling

--- events.go
+++ events.go
@@ -4,12 +4,52 @@
 
 package main
 
-import ()
+import (
+  "context"
+  "github.com/ThreeDotsLabs/watermill"
+  "github.com/ThreeDotsLabs/watermill-kafka/v2/pkg/kafka"
+  "github.com/ThreeDotsLabs/watermill/message"
+  pb "github.com/ckbball/smurfin-vendor/proto/vendor"
+  "log"
+  "time"
+)
 
 // to catalog
-type TakeAccountDownEvent struct {
+type AccountTakenDownEvent struct {
+  Id string
 }
 
 // to agent
-type NewAccountEvent struct {
+type AccountSubmittedEvent struct {
+  Account *pb.Account
+}
+
+func InitSubscriber(config kafka.SubscriberConfig) *kafka.Subscriber {
+  subscriber, err := kafka.NewSubscriber(
+    kafka.SubscriberConfig{
+      Brokers:               []string{"kafka:9092"},
+      Unmarshaler:           kafka.DefaultMarshaler{},
+      OverwriteSaramaConfig: config,
+      ConsumerGroup:         "test_consumer_group",
+    },
+    watermill.NewStdLogger(false, false),
+  )
+  if err != nil {
+    panic(err)
+  }
+  return subscriber
+}
+
+func InitPublisher() *kafka.Publisher {
+  publisher, err := kafka.NewPublisher(
+    kafka.PublisherConfig{
+      Brokers:   []string{"kafka:9092"},
+      Marshaler: kafka.DefaultMarshaler{},
+    },
+    watermill.NewStdLogger(false, false),
+  )
+  if err != nil {
+    panic(err)
+  }
+  return publisher
 }
